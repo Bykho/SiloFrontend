97994fe6d1a96e6eb03465c342d7c01ac1dc4c3c
Nico Bykhovsky <nico@bykhovsky.com>
2024-06-26 12:29:06 -0400

    test see if works

--- src/frontend/pages/LoginPage/Login.js
+++ src/frontend/pages/LoginPage/Login.js
@@ -40,7 +40,7 @@ function Login() {
           <div className={styles.buttonContainer}>
             <button className={`${styles.button} ${styles.loginButton}`} onClick={toggleLoginForm}>Login</button>
             <a className={`${styles.button} ${styles.createButton}`} href="/SignUp">
-              Create
+              Create1
             </a>
           </div>
         ) : (
