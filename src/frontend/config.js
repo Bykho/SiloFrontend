d340b8265ecece6fbc0a1ab4ffadb9cca88c5657
Nico Bykhovsky <nico@bykhovsky.com>
2024-06-27 00:45:53 -0400

    r

--- src/frontend/config.js
+++ src/frontend/config.js
@@ -6,7 +6,7 @@
 
 // config.js
 const config = {
-    apiBaseUrl: "https://dummyflaska-b17a47997732.herokuapp.com/"
+    apiBaseUrl: "https://dummyflaska-b17a47997732.herokuapp.com"
 };
 
 export default config;
