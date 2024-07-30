0d929215bdb59de013f13eaa85c2361473298643
dringness <daniel.ringness@gmail.com>
2024-07-30 15:57:06 -0400

    added ToS

--- src/frontend/pages/SignUpPage/SignUp.js
+++ src/frontend/pages/SignUpPage/SignUp.js
@@ -9,6 +9,7 @@ import config from '../../config';
 import SuggestedPortfolio from '../../components/SuggestedPortfolio';
 import pdfToText from 'react-pdftotext';
 
+
 function SignUp() {
   const [page, setPage] = useState(1);
   const [formData, setFormData] = useState({
@@ -41,6 +42,9 @@ function SignUp() {
   const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
   const [selectedPortfolio, setSelectedPortfolio] = useState([]);
   const [savedUsersId, setSavedUsersId] = useState('');
+  const [agreedToTerms, setAgreedToTerms] = useState(false);
+  const [showTermsModal, setShowTermsModal] = useState(false);
+
   const [isLoading, setIsLoading] = useState(false); // Add loading state
 
   const { user, updateUser } = useUser();
@@ -130,7 +134,7 @@ function SignUp() {
       case 2:
         return formData.university && formData.grad && formData.major;
       case 3:
-        return formData.interests.length && formData.skills.length && formData.biography;
+        return formData.interests.length && formData.skills.length && formData.biography && agreedToTerms;
       case 4:
         return true;
       default:
@@ -140,8 +144,8 @@ function SignUp() {
 
   useEffect(() => {
     setIsNextDisabled(!validatePage(page));
-    setIsSubmitDisabled(!validatePage(3));
-  }, [formData, page]);
+    setIsSubmitDisabled(!validatePage(3) || !agreedToTerms);
+  }, [formData, page, agreedToTerms]);
 
   useEffect(() => {
     if (Object.keys(suggestedSummary).length > 0) {
@@ -217,6 +221,20 @@ function SignUp() {
       console.error('Error during registration:', error);
       setError('An unexpected error occurred. Please try again.');
     }
+  };
+
+
+  const handleTermsChange = (e) => {
+    setAgreedToTerms(e.target.checked);
+  };
+
+  const openTermsModal = (e) => {
+    e.preventDefault();
+    setShowTermsModal(true);
+  };
+
+  const closeTermsModal = () => {
+    setShowTermsModal(false);
   };
 
 
@@ -458,6 +476,17 @@ function SignUp() {
                 required
               />
             </div>
+            <div className={styles.tosForm}>
+              <label className={styles.checkboxLabel}>
+                <input
+                  type="checkbox"
+                  checked={agreedToTerms}
+                  onChange={handleTermsChange}
+                  required
+                />
+                I agree to the <a href="#" onClick={openTermsModal} className={styles.tos}> Terms of Service</a>
+              </label>
+            </div>
             <div className={styles.buttonGroup}>
               <button type="button" onClick={handleBack} className={styles.btnBack}>Back</button>
               <button 
@@ -507,6 +536,79 @@ function SignUp() {
           </form>
         </div>
       </div>
+      {showTermsModal && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <h2>Terms of Service</h2>
+            <div className={styles.termsContent}>
+              <p>Last Updated: 7/30/24</p>
+              <p>Welcome to Silo!</p>
+              <p>These Terms of Service ("Terms") govern your use of our website, services, and applications (the "Service"). By accessing or using the Service, you agree to be bound by these Terms.</p>
+              
+              <h3>1. Acceptance of Terms</h3>
+              <p>By using the Service, you agree to comply with and be legally bound by these Terms, whether or not you become a registered user of the Service. If you do not agree to these Terms, you may not use the Service.</p>
+              
+              <h3>2. Eligibility</h3>
+              <p>You must be at least 18 years old to use the Service. By using the Service, you represent and warrant that you are 18 or older and have the full right, power, and authority to enter into these Terms and to fully perform all of your obligations hereunder.</p>
+              
+              <h3>3. Account Registration</h3>
+              <p>To use certain features of the Service, you must register for an account. When you register, you agree to provide accurate, current, and complete information about yourself and to update such information to keep it accurate, current, and complete. You are responsible for safeguarding your password and for all activities that occur under your account.</p>
+              
+              <h3>4. User Content</h3>
+              <p>You are responsible for any content you upload, post, or otherwise transmit through the Service ("User Content"). You retain ownership of your User Content. By uploading User Content, you grant Silo a worldwide, non-exclusive, royalty-free, sublicensable, and transferable license to use, reproduce, distribute, prepare derivative works of, display, and perform the User Content solely in connection with providing and promoting the Service. This license does not grant Silo any rights to sell your User Content or otherwise use it for any purposes unrelated to the Service.</p>
+              
+              <h3>5. Prohibited Activities</h3>
+              <p>You agree not to:</p>
+              <ul>
+                <li>Use the Service for any illegal purpose or in violation of any local, state, national, or international law.</li>
+                <li>Post any User Content that is unlawful, harmful, threatening, abusive, harassing, defamatory, vulgar, obscene, libelous, or otherwise objectionable.</li>
+                <li>Impersonate any person or entity or falsely state or otherwise misrepresent your affiliation with a person or entity.</li>
+                <li>Upload, post, or otherwise transmit any content that infringes upon the intellectual property rights of any third party.</li>
+              </ul>
+              
+              <h3>6. Privacy</h3>
+              <p>Silo is committed to protecting your privacy. This Privacy Policy explains how we collect, use, and share information about you when you use our website, services, and applications (the "Service").
+                  1. Information We Collect
+                  Personal Information: When you register for an account, we may collect personal information such as your name, email address, and other contact information.
+                  Usage Information: We collect information about your use of the Service, including the type of browser you use, access times, pages viewed, your IP address, and the page you visited before navigating to our Service.
+                  User Content: Any content you upload, post, or otherwise transmit through the Service.
+                  2. How We Use Your Information
+                  To provide, maintain, and improve the Service.
+                  To communicate with you, including sending you updates, security alerts, and administrative messages.
+                  To personalize your experience on the Service.
+                  To monitor and analyze trends, usage, and activities in connection with the Service.
+                  To protect the security and integrity of the Service.
+                  3. Sharing Your Information
+                  We do not share your personal information with third parties except in the following circumstances:
+                  With Your Consent: We may share or disclose your information with your consent.
+                  Service Providers: We may share your information with third-party vendors, consultants, and other service providers who need access to such information to carry out work on our behalf.
+                  Legal Requirements: We may disclose your information to comply with applicable laws, regulations, or legal processes.
+                  4. Data Security
+                  We take reasonable measures to protect your information from unauthorized access, use, alteration, or destruction. However, no internet or email transmission is ever fully secure or error-free.
+                  5. Your Choices
+                  Account Information: You may update, correct, or delete your account information at any time by logging into your account.
+                  Cookies: Most web browsers are set to accept cookies by default. You can usually choose to set your browser to remove or reject cookies.
+              </p>
+              
+              <h3>7. Termination</h3>
+              <p>We may terminate or suspend your account and access to the Service at our sole discretion, without prior notice or liability, for any reason, including if you breach these Terms. Upon termination, your right to use the Service will immediately cease.</p>
+              
+              <h3>8. Disclaimers</h3>
+              <p>The Service is provided "as is" and "as available" without warranties of any kind, either express or implied. Silo does not warrant that the Service will be uninterrupted or error-free, that defects will be corrected, or that the Service is free of viruses or other harmful components.</p>
+              
+              <h3>9. Limitation of Liability</h3>
+              <p>In no event shall Silo be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any loss of data, use, goodwill, or other intangible losses, resulting from (i) your use of or inability to use the Service; (ii) any unauthorized access to or use of our servers and/or any personal information stored therein; (iii) any interruption or cessation of transmission to or from the Service.</p>
+              
+              <h3>10. Changes to Terms</h3>
+              <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material, we will provide at least 30 days' notice prior to any new terms taking effect. By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms.</p>
+              
+              <h3>11. Governing Law</h3>
+              <p>These Terms shall be governed and construed in accordance with the laws of the state of New York, without regard to its conflict of law provisions.</p>
+            </div>
+            <button onClick={closeTermsModal} className={styles.closeButton}>x</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
