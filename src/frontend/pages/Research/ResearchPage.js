d9cc7205f616f2e6d88933f247370e9f29036948
dringness <daniel.ringness@gmail.com>
2024-09-23 22:54:25 -0400

    added icon

--- src/frontend/pages/Research/ResearchPage.js
+++ src/frontend/pages/Research/ResearchPage.js
@@ -4,6 +4,8 @@ import styles from './ResearchPage.module.css';
 import config from '../../config';
 import LoadingIndicator from '../../components/LoadingIndicator';
 import { FaRegBookmark, FaBookmark, FaSearch } from "react-icons/fa";
+import { FaBookBookmark } from "react-icons/fa6";
+
 
 const ResearchPage = () => {
   const navigate = useNavigate();
@@ -116,7 +118,7 @@ const ResearchPage = () => {
   const SavedPapersModal = () => (
     <div className={styles.modal}>
       <div className={styles.modalContent}>
-        <h2>Saved Papers</h2>
+        <h2> Saved Papers</h2>
         <ul>
           {savedPapers.map((paper, index) => (
             <li key={index}>
@@ -158,7 +160,7 @@ const ResearchPage = () => {
           </button>
         </form>
         <button className={styles.savedPapersButton} onClick={() => setShowModal(true)}>
-          Saved Papers
+        <FaBookBookmark /> Saved Papers
         </button>
       </div>
       <main className={styles.mainContent}>
